dd84752072a04c325d03426b6a3b19d139d22018
mhamdouchi <career@hamdouchi.com>
2022-06-26 11:40:47 -0400

    Adds project structure

---
+++ TableTennisTracking/iOS/Views/ViewControllers/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  TableTennisTracking
+//
+//  Created by Mohamed Hamdouchi on 6/26/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {}
+
+// MARK: - Lifecycle
+extension MainViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
